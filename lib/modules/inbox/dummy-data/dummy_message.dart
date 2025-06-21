f3ae93d597463548d47857cc2229227a03ab1b53
Rashedul Islam Hridoy <133196899+selisehridoy@users.noreply.github.com>
2025-06-21 22:32:27 +0600

    feat: dummy chat optimised

---
+++ lib/modules/inbox/dummy-data/dummy_message.dart
@@ -0,0 +1,9 @@
+final dummy_messages = [
+  {'text': 'Yes I\'m on the train now', 'time': '14:21', 'isMe': false},
+  {'text': 'I\'m here', 'time': '14:22', 'isMe': false},
+  {'text': 'Can you come and get me', 'time': '14:22', 'isMe': false},
+  {'text': 'are you ok?', 'time': '14:21', 'isMe': true},
+  {'text': 'You were probably late', 'time': '14:23', 'isMe': true},
+  {'text': 'Just outside the office', 'time': '14:24', 'isMe': true},
+  {'text': 'I\'m so hungry', 'time': '14:26', 'isMe': false},
+];
--- lib/modules/inbox/pages/chat_inbox_screen.dart
+++ lib/modules/inbox/pages/chat_inbox_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/modules/inbox/dummy-data/dummy_message.dart';
 
 class ChatInboxScreen extends StatelessWidget {
   final String name;
@@ -7,15 +8,7 @@ class ChatInboxScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final messages = [
-      {'text': 'Yes I\'m on the train now', 'time': '14:21', 'isMe': false},
-      {'text': 'I\'m here', 'time': '14:22', 'isMe': false},
-      {'text': 'Can you come and get me', 'time': '14:22', 'isMe': false},
-      {'text': 'are you ok?', 'time': '14:21', 'isMe': true},
-      {'text': 'You were probably late', 'time': '14:23', 'isMe': true},
-      {'text': 'Just outside the office', 'time': '14:24', 'isMe': true},
-      {'text': 'I\'m so hungry', 'time': '14:26', 'isMe': false},
-    ];
+    final messages = dummy_messages;
 
     return Scaffold(
       appBar: AppBar(
